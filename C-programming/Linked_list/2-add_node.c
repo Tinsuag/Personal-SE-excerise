fda708c5dbdecde0a9b1f9d5430238ac02e076a7
Tinsuag <tinsaeareg.work@gmail.com>
2023-08-02 23:53:55 +0300

    final touchs

--- C-programming/Linked_list/2-add_node.c
+++ C-programming/Linked_list/2-add_node.c
@@ -1,10 +1,20 @@
 #include "li_t.h"
+#include <string.h>
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *temp = malloc(sizeof(list_t));
-	temp->str = *str;
-	temp->next = NULL;
-	
-	head->next = temp;
+	char str2;
+	int size = 0;
+
+	str2 = strdup(*str);
+	temp->str = str2;
+	temp->next = *head;
+	while (*str2)
+	{
+		size++;
+		str2++;
+	}
+	temp->len = size;
+	*head = temp;
 	return (temp);
 }
