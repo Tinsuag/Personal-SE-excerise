a42e566fb4016149b568ba8d7e67b0d3430412b2
Tinsuag <tinsaeareg.work@gmail.com>
2023-08-02 23:28:02 +0300

    task 2

--- C-programming/Linked_list/1-list_len.c
+++ C-programming/Linked_list/1-list_len.c
@@ -1,9 +1,14 @@
-#include <li_t.h>
-size_t list_len(const _list_t *h)
+#include "li_t.h"
+size_t list_len(const list_t *h)
 {
+	int count = 0;
 
-}#include <li_t.h>
-size_t list_len(const _list_t *h)
-{
-
+	while (h)
+	{
+		if (h->str == NULL)
+			break;
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
